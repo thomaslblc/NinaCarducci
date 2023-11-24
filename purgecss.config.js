fbea498df546c33f9c4cfe2cb22330b393d266aa
Thomas <thomaslblc@gmail.com>
2023-11-24 09:08:52 +0100

    projet finalisé

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['index.html', '**/*.js', '**/*.html'],
+    css: ['./assets/bootstrap/bootstrap.css'],
+    safelist: ['col-12', 'col-sm-6', 'col-md-4', 'col-lg-4', 'col-xl-4']
+  }
